d0a3c4a2a014e2c5380de7eb91fbe332113c3d4a
Philipp Hager <philipp.hager@student.hpi.uni-potsdam.de>
2020-01-09 09:11:43 +0100

    Add requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@
 
 from setuptools import setup, find_packages
 
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+unparsed_requirements = parse_requirements('requirements.txt', session=False)
+requirements = [str(ir.req) for ir in unparsed_requirements]
+
 setup(name='sif',
       version='0.0.1',
       description='This package has shared components.',
@@ -9,4 +19,5 @@ setup(name='sif',
       author_email='',
       packages=find_packages(exclude=["examples*", "data*", "auxiliary_data*"]),
       license='LICENSE',
-    )
+      install_requires=requirements
+      )
